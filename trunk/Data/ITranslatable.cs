79c38f720df90566c8cfe9356458c9281b96a698
i.deruga <i.deruga@9f8292cc-0193-11de-85c2-bb0660c75f22>
2009-02-28 17:04:26 +0000

    added a picture and some dictionary words

---
+++ trunk/Data/ITranslatable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arabic_Keyboard_Tutor.Data
+{
+    interface ITranslatable
+    {
+        string word { get; }
+        string translation { get; }
+        string transcription { get; }
+    }
+}
--- trunk/Properties/AssemblyInfo.cs
+++ trunk/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Arabic Keyboard Tutor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("luxoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Arabic Keyboard Tutor")]
-[assembly: AssemblyCopyright("Copyright © luxoft 2009")]
+[assembly: AssemblyCopyright("Copyright © Igor Deruga 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2")]
+[assembly: AssemblyFileVersion("1.2")]
